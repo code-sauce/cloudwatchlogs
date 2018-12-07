bf48bf280abd8912781bc241fd9d59442c242eab
Saurabh Jain <sjain@samsungnext.com>
2018-12-07 15:24:24 -0800

    more tweaks

--- cloudwatch/consumer_mixpanel.py
+++ cloudwatch/consumer_mixpanel.py
@@ -1,9 +1,22 @@
 from cloudwatch.consumer_abstract import BaseConsumer
+import json
 
 
 class MixpanelConsumer(BaseConsumer):
 
     def process(self, log_line, log_group, log_stream):
-        print("MIXPANELLLLL")
-        print(log_line)
-        print(type(log_line))
+
+        try:
+            message = log_line['message']
+            message = json.loads(message)
+            message = message.get('message')
+            message = json.loads(message)
+            templatized_url = message.get('templatized_url')
+            if not templatized_url:
+                return
+            app_id = message.get('app_id')
+
+            # TODO - fire events to mixpanel
+            print("firing {} and {} to mixpanel".format(templatized_url, app_id))
+        except Exception as ex:
+            pass
